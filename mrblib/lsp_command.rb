e27a9b176969144751ea9e202bcbd1af06251cfa
masahino <masahino@gmail.com>
2019-06-29 17:35:04 +0900

    support formatting

--- mrblib/lsp_command.rb
+++ mrblib/lsp_command.rb
@@ -27,5 +27,69 @@ module Mrbmacs
     def lsp_goto_definition()
       lsp_goto_command("definition")
     end
+
+    def lsp_edit_buffer(text_edit)
+      sci_begin_undo_action()
+      text_edit.reverse_each do |e|
+        @logger.debug e
+        @frame.view_win.sci_set_sel(
+          @frame.view_win.sci_findcolumn(e['range']['start']['line'], e['range']['start']['character']),
+          @frame.view_win.sci_findcolumn(e['range']['end']['line'], e['range']['end']['character']))
+        sci_replace_sel("", e['newText'])
+      end
+      sci_end_undo_action()
+    end
+
+    def lsp_formatting()
+      @logger.debug "lsp_formatting"
+      lang = @current_buffer.mode.name
+      if @ext.lsp[lang] != nil and @ext.lsp[lang].status == :running
+        td = LSP::Parameter::TextDocumentIdentifier.new(@current_buffer.filename)
+        param = {"textDocument" => td, "options" => {
+            "tabSize" => @current_buffer.mode.indent,
+            "insertSpaces" => !@current_buffer.mode.use_tab
+          }
+        }
+        @ext.lsp[lang].formatting(param) do |resp|
+          @logger.debug "resp"
+          @logger.debug resp
+          if resp != nil
+            lsp_edit_buffer(resp['result'])
+          end
+        end
+      end
+    end
+
+    def lsp_range_formatting()
+      @logger.debug "lsp_range_formatting"
+      lang = @current_buffer.mode.name
+      if @ext.lsp[lang] != nil and @ext.lsp[lang].status == :running
+        @logger.debug "lsp_range_formatting go"
+        td = LSP::Parameter::TextDocumentIdentifier.new(@current_buffer.filename)
+        anchor_line, anchor_col = get_current_line_col(@mark_pos)
+        @logger.debug "anchor_line:" + anchor_line.to_s
+        @logger.debug "anchor_col:" + anchor_col.to_s
+        current_line, current_col = get_current_line_col(sci_get_current_pos())
+        @logger.debug "curr_line:" + current_line.to_s
+        @logger.debug "curr_col:" + current_col.to_s
+        param = {"textDocument" => td, "options" => {
+            "range" => {
+              "start" => {"line" => anchor_line, "character" => anchor_col},
+              "end" => {"line" => current_line, "character" => current_col}
+            },
+            "tabSize" => @current_buffer.mode.indent,
+            "insertSpaces" => !@current_buffer.mode.use_tab
+          }
+        }
+        @logger.debug param
+        @ext.lsp[lang].rangeFormatting(param) do |resp|
+          @logger.debug "resp"
+          @logger.debug resp
+          if resp != nil
+            lsp_edit_buffer(resp['result'])
+          end
+        end
+      end
+    end
   end
 end
