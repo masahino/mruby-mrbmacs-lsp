2f85064863bdb6993fce17387f8b1745121b6b7e
masahino <masahino@gmail.com>
2021-09-30 07:01:02 +0900

    set severity of annotation message

--- mrblib/lsp.rb
+++ mrblib/lsp.rb
@@ -1,4 +1,5 @@
 module Mrbmacs
+  # LspExtension
   class LspExtension < Extension
     LSP_LIST_TYPE = 99
     LSP_DEFAULT_CONFIG = {
@@ -43,39 +44,40 @@ module Mrbmacs
       'M-r' => 'lsp-references',
       'M-d' => 'lsp-definition'
     }
-
-    def self.register_lsp_client(app)
-      app.use_builtin_completion = false
-      app.ext.data['lsp'] = {}
+    def self.register_lsp_client(appl)
+      appl.use_builtin_completion = false
+      appl.ext.data['lsp'] = {}
       config = LSP_DEFAULT_CONFIG
-      if app.config.ext['lsp'] != nil
-        config.merge! app.config.ext['lsp']
+      if appl.config.ext['lsp'] != nil
+        config.merge! appl.config.ext['lsp']
       end
       config.each do |l, v|
         if Which.which(v['command']) != nil
-          app.ext.data['lsp'][l] = LSP::Client.new(v['command'], v['options'])
-          Mrbmacs::LspExtension.set_keybind(app, l)
+          appl.ext.data['lsp'][l] = LSP::Client.new(v['command'], v['options'])
+          Mrbmacs::LspExtension.set_keybind(appl, l)
         end
       end
-      app.add_command_event(:after_find_file) do |app, filename|
+      appl.add_command_event(:after_find_file) do |app, filename|
         lang = app.current_buffer.mode.name
         if app.ext.data['lsp'][lang] != nil
           if app.ext.data['lsp'][lang].status == :stop
-            app.ext.data['lsp'][lang].start_server({
-                'rootUri' => 'file://' + app.current_buffer.directory,
+            app.ext.data['lsp'][lang].start_server(
+              {
+                'rootUri' => "file://#{app.current_buffer.directory}",
                 'capabilities' => {
                   'workspace' => {},
                   'textDocument' => {
-#                    'hover' => {
-#                      'contentFormat' => 'plaintext',
-#                    },
+                    #                    'hover' => {
+                    #                      'contentFormat' => 'plaintext',
+                    #                    },
                   },
                   'trace' => 'verbose'
                 }
-              })
+              }
+            )
             if app.ext.data['lsp'][lang].io != nil
-              app.add_io_read_event(app.ext.data['lsp'][lang].io) do |app, io|
-                app.lsp_read_message(io)
+              app.add_io_read_event(app.ext.data['lsp'][lang].io) do |iapp, io|
+                iapp.lsp_read_message(io)
               end
             end
           end
@@ -87,7 +89,7 @@ module Mrbmacs
         end
       end
 
-      app.add_command_event(:after_save_buffer) do |app, filename|
+      appl.add_command_event(:after_save_buffer) do |app, filename|
         lang = app.current_buffer.mode.name
         if app.lsp_is_running?
           app.ext.data['lsp'][lang].didSave(
@@ -95,11 +97,12 @@ module Mrbmacs
               'textDocument' => LSP::Parameter::TextDocumentIdentifier.new(filename)
             }
           )
+          app.message('didSave')
         end
       end
 
-      app.add_command_event(:before_save_buffers_kill_terminal) do |app|
-        app.ext.data['lsp'].each do |lang, client|
+      appl.add_command_event(:before_save_buffers_kill_terminal) do |app|
+        app.ext.data['lsp'].each do |_lang, client|
           if client.status != :stop
             client.shutdown
             client.stop_server
@@ -107,7 +110,7 @@ module Mrbmacs
         end
       end
 
-      app.add_sci_event(Scintilla::SCN_CHARADDED) do |app, scn|
+      appl.add_sci_event(Scintilla::SCN_CHARADDED) do |app, scn|
         lang = app.current_buffer.mode.name
         if app.lsp_is_running?
           app.lsp_sync_text
@@ -118,7 +121,7 @@ module Mrbmacs
         end
       end
 
-      app.add_sci_event(Scintilla::SCN_UPDATEUI) do |app, scn|
+      appl.add_sci_event(Scintilla::SCN_UPDATEUI) do |app, scn|
         lang = app.current_buffer.mode.name
         if app.lsp_is_running?
           if scn['updated'] & Scintilla::SC_UPDATE_CONTENT == Scintilla::SC_UPDATE_CONTENT
@@ -133,17 +136,17 @@ module Mrbmacs
         end
       end
 
-      app.add_sci_event(Scintilla::SCN_DWELLSTART) do |app, scn|
+      appl.add_sci_event(Scintilla::SCN_DWELLSTART) do |app, scn|
         lang = app.current_buffer.mode.name
         if app.lsp_is_running?
-          line, col = app.get_current_line_col(scn['pos'])
+          _line, _col = app.get_current_line_col(scn['pos'])
           td = LSP::Parameter::TextDocumentIdentifier.new(app.current_buffer.filename)
           param = { 'textDocument' => td, 'position' => app.lsp_position(scn['pos']) }
           app.ext.data['lsp'][lang].hover(param)
         end
       end
 
-      app.add_sci_event(Scintilla::SCN_USERLISTSELECTION) do |app, scn|
+      appl.add_sci_event(Scintilla::SCN_USERLISTSELECTION) do |app, scn|
         if scn['list_type'] == LSP_LIST_TYPE
           target_file, lines, col = scn['text'].split(',')
           if app.current_buffer.filename != target_file
@@ -154,7 +157,7 @@ module Mrbmacs
       end
     end
 
-    def self.set_keybind(app, lang)
+    def self.set_keybind(_app, lang)
       mode = Mrbmacs::Mode.get_mode_by_name(lang)
       if mode != nil
         LSP_DEFAULT_KEYMAP.each do |k, v|
@@ -162,8 +165,24 @@ module Mrbmacs
         end
       end
     end
+
+    def self.get_diagnostic_severity_to_s(severity)
+      case severity
+      when 1
+        'Error'
+      when 2
+        'Warning'
+      when 3
+        'Information'
+      when 4
+        'Hint'
+      else
+        'Unknwon'
+      end
+    end
   end
 
+  # Application
   class Application
     def lsp_is_running?
       lang = @current_buffer.mode.name
@@ -202,101 +221,99 @@ module Mrbmacs
     end
 
     def lsp_read_message(io)
-      @ext.data['lsp'].each_pair do |k, v|
-        if io == v.io
-          resp = v.recv_message[1]
-          @logger.debug resp.to_s
-          if resp['id'] != nil
-            # request or response
-            id = resp['id'].to_i
-            if v.request_buffer[id] != nil 
-              @logger.debug v.request_buffer[id][:message]['method'].to_s
-              case v.request_buffer[id][:message]['method']
-              when 'initialize'
-                v.initialized(resp)
-                v.didOpen(
-                  { 'textDocument' => LSP::Parameter::TextDocumentItem.new(@current_buffer.filename) }
-                )
-                @current_buffer.additional_info = v.server[:command] + ':' + v.status.to_s[0]
-              when 'textDocument/completion'
-                if @frame.view_win.sci_autoc_active == 0 and @frame.view_win.sci_calltip_active == 0
-                  @logger.debug v.request_buffer[id].to_s
-                  len, candidates = lsp_get_completion_list(v.request_buffer[id][:message]['params'], resp)
-                  if candidates.length > 0
-                    @frame.view_win.sci_autoc_show(len, candidates)
-                  end
+      @ext.data['lsp'].each_pair do |_k, v|
+        next unless io == v.io
+
+        # if io == v.io
+        resp = v.recv_message[1]
+        @logger.debug resp.to_s
+        if resp['id'] != nil
+          # request or response
+          id = resp['id'].to_i
+          if v.request_buffer[id] != nil
+            @logger.debug v.request_buffer[id][:message]['method'].to_s
+            case v.request_buffer[id][:message]['method']
+            when 'initialize'
+              v.initialized(resp)
+              v.didOpen(
+                { 'textDocument' => LSP::Parameter::TextDocumentItem.new(@current_buffer.filename) }
+              )
+              @current_buffer.additional_info = v.server[:command] + ':' + v.status.to_s[0]
+            when 'textDocument/completion'
+              if @frame.view_win.sci_autoc_active == 0 and @frame.view_win.sci_calltip_active == 0
+                @logger.debug v.request_buffer[id].to_s
+                len, candidates = lsp_get_completion_list(v.request_buffer[id][:message]['params'], resp)
+                if candidates.length > 0
+                  @frame.view_win.sci_autoc_show(len, candidates)
                 end
-              when 'textDocument/hover'
-                if frame.view_win.sci_autoc_active == 0
-                  if resp['result'] != nil and resp['result']['contents']['value'] != nil
-                    markup_kind = resp['result']['contents']['kind']
-                    value = resp['result']['contents']['value']
-                    if value.size > 0
-                      @frame.view_win.sci_calltip_show(@frame.view_win.sci_get_current_pos, value)
-                    end
-                  end
-                end
-              when 'textDocument/signatureHelp'
-                @logger.debug resp['result']['signatures'].to_s
-                if resp['result'] != nil and resp['result']['signatures'] != nil
-                  list = resp['result']['signatures'].map { |s| s['label'] }.uniq
-                  @logger.debug list.to_s
-                  if list.size > 0
-                    @frame.view_win.sci_calltipshow(@frame.view_win.sci_get_current_pos, list.join("\n"))
-                  end
-                end
-              else
-                @logger.info 'unknown message'
-                @logger.info resp.to_s
               end
-              v.request_buffer.delete(id)
-            else # request?
-              @logger.info resp.to_s
-            end
-          else # notification
-            case resp['method']
-            when 'textDocument/publishDiagnostics'
-              @logger.info 'publishDiagnostics'
-              if @current_buffer.filename == lsp_uri_to_path(resp['params']['uri'])
-                lsp_show_annotation(resp['params']['diagnostics'])
+            when 'textDocument/hover'
+              if frame.view_win.sci_autoc_active == 0
+                if resp['result'] != nil and resp['result']['contents']['value'] != nil
+                  markup_kind = resp['result']['contents']['kind']
+                  value = resp['result']['contents']['value']
+                  if value.size > 0
+                    @frame.view_win.sci_calltip_show(@frame.view_win.sci_get_current_pos, value)
+                  end
+                end
+              end
+            when 'textDocument/signatureHelp'
+              @logger.debug resp['result']['signatures'].to_s
+              if resp['result'] != nil and resp['result']['signatures'] != nil
+                list = resp['result']['signatures'].map { |s| s['label'] }.uniq
+                @logger.debug list.to_s
+                if list.size > 0
+                  @frame.view_win.sci_calltipshow(@frame.view_win.sci_get_current_pos, list.join("\n"))
+                end
               end
             else
-              @logger.info "unknown method #{resp.to_s}"
+              @logger.info 'unknown message'
               @logger.info resp.to_s
             end
+            v.request_buffer.delete(id)
+          else # request?
+            @logger.info resp.to_s
           end
-          break
+        else # notification
+          case resp['method']
+          when 'textDocument/publishDiagnostics'
+            @logger.info 'publishDiagnostics'
+            if @current_buffer.filename == lsp_uri_to_path(resp['params']['uri'])
+              lsp_show_annotation(resp['params']['diagnostics'])
+            end
+          else
+            @logger.info "unknown method #{resp}"
+            @logger.info resp.to_s
+          end
         end
+        break
       end
+      # end
     end
 
     def lsp_send_completion_request(scn)
       lang = @current_buffer.mode.name
       if lsp_is_running?
-        line, col = get_current_line_col
+        line, col = get_current_line_col()
         if col > 0
           line_text = get_current_line_text.chomp[0..col]
-          input = line_text.split(' ').pop
+          input = line_text.split(" ").pop
           td = LSP::Parameter::TextDocumentIdentifier.new(@current_buffer.filename)
           if input != nil and input.length > 0
-            if lsp_signature_trigger_characters.include?(scn['ch'].chr('UTF-8'))
-              @ext.data['lsp'][lang].signatureHelp(
-                { 'textDocument' => td, 'position' => lsp_position }
-              )
+            if lsp_signature_trigger_characters.include?(scn['ch'].chr("UTF-8"))
+              @ext.data['lsp'][lang].signatureHelp({
+                  "textDocument" => td, "position" => lsp_position})
             else
-              if lsp_completion_trigger_characters.include?(scn['ch'].chr('UTF-8'))
+              if lsp_completion_trigger_characters.include?(scn['ch'].chr("UTF-8"))
                 trigger_kind = 2
-                trigger_char = scn['ch'].chr('UTF-8')
+                trigger_char = scn['ch'].chr("UTF-8")
               else
                 trigger_kind = 1
-                trigger_char = ''
+                trigger_char = ""
               end
-              param = {
-                'textDocument' => td,
-                'position' => lsp_position,
-                'context' => {
-                  'triggerKind' => trigger_kind, 'triggerCharacter' => trigger_char
-                }
+              param = {"textDocument" => td,
+                "position" => lsp_position,
+                "context" => {"triggerKind" => trigger_kind, "triggerCharacter" => trigger_char},
               }
               @logger.debug param.to_s
               @ext.data['lsp'][lang].completion(param)
@@ -304,20 +321,20 @@ module Mrbmacs
           end
         end
       else
-        @logger.info 'not yet initialized'
+        @logger.info "not yet initialized"
       end
     end
 
     def lsp_get_completion_list(req, res)
-      line, col = get_current_line_col
-      line_text = get_current_line_text.chomp[0..col]
+      line, col = get_current_line_col()
+      line_text = get_current_line_text().chomp[0..col]
       input = if req.has_key?('context') &&
         req['context'].has_key?('triggerKind') &&
         req['context']['triggerKind'] == 2
-                ''
-              else
-                line_text.split(/[ #{lsp_completion_trigger_characters.join}]/).pop
-              end
+        ""
+      else
+        line_text.split(/[ #{lsp_completion_trigger_characters.join}]/).pop
+      end
       if res.has_key?('result')
         items = []
         if res['result'].is_a?(Hash)
@@ -328,8 +345,8 @@ module Mrbmacs
           items = res['result']
         end
 #        candidates = res['result']['items'].map { |h|
-        candidates = items.map do |h|
-          str = ''
+        candidates = items.map { |h|
+          str = ""
           if h['textEdit'] != nil
             str = h['textEdit']['newText'].strip
           elsif h['insertText'] != nil
@@ -338,7 +355,7 @@ module Mrbmacs
             str = h['label'].strip
           end
           str
-        end
+        }
       else
         candidates = []
       end
@@ -346,14 +363,31 @@ module Mrbmacs
       [input.length, candidates.sort.uniq.join(' ')]
     end
 
+    def lsp_get_style_from_severity(severity)
+      case severity
+      when 1
+        @theme.annotation_style(:error)
+      when 2
+        @theme.annotation_style(:warn)
+      when 3
+        @theme.annotation_style(:info)
+      when 4
+        @theme.annotation_style(:info)
+      else
+        @theme.annotation_style(:other)
+      end
+    end
+
     def lsp_show_annotation(diagnostics)
       @frame.view_win.sci_annotation_clearall
       diagnostics.each do |d|
-        @frame.show_annotation(
-          d['range']['start']['line'] + 1,
-          d['range']['start']['character'] + 1,
-          d['message']
-        )
+        line = d['range']['start']['line'] + 1
+        col = d['range']['start']['character'] + 1
+        message = Mrbmacs::LspExtension.get_diagnostic_severity_to_s(d['severity']) + ':' + d['message']
+        if @frame.view_win.sci_annotation_get_lines(line) > 0
+          message = @frame.view_win.sci_annotation_get_text(line) + "\n" + message
+        end
+        @frame.show_annotation(line, col, message, lsp_get_style_from_severity(d['severity']))
       end
       @frame.view_win.sci_scrollcaret
     end
